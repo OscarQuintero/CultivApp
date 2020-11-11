e41f080fcb1eed9d13f58506632bb0eb76107c21
OscarQuintero <oscar24quintero@hotmail.com>
2020-11-11 17:44:38 -0600

    Primer commit

---
+++ CultivApp/CultivApp/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  CultivApp
+//
+//  Created by Oscar Quintero on 08/11/20.
+//
+
+import UIKit
+
+
+
+
+class ViewController: UIViewController {
+    
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func buttonA(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func buttonB(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonD(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonC(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
